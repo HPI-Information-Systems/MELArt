83136c1de24ca3c03ed879c08a0d12896aa3b7d5
Alejandro Sierra <Alejandro.Sierra@hpi.de>
2024-09-24 10:54:28 +0200

    Include siteLinks and proper types for the qid and sitelink

--- index_wikidata_labels.py
+++ index_wikidata_labels.py
@@ -14,25 +14,29 @@ def process_batch(offset):
         if len(qids)==0:
             return None
         labels=sq.sparql_all_lables(qids)
+        site_links=sq.sparql_sitelinks(qids)
         documents = []
         for qid, (label,alt) in labels.items():
             #if no character in label or any of the alt labels is an uppercase letter, skip
             if not any(c.isupper() for c in label) and not any(any(c.isupper() for c in alt_label) for alt_label in alt):
                 #print(f"Skipping {qid} because no uppercase letter in label or alt labels")
                 continue
-            
+            site_links_count=site_links[qid]
             doc={
                 "id": qid,
-                "qid": qid,
-                "label_txt_en": label
+                "qid_s": qid,
+                "label_txt_en": label,
+                "sitelinks_i": site_links_count
             }
             documents.append(doc)
             for i,label in enumerate(alt):
                 id=f"{qid}_{i}"
                 doc={
                     "id": id,
+                    "qid_s": qid,
                     "qid": qid,
-                    "label_txt_en": label
+                    "label_txt_en": label,
+                    "sitelinks_i": site_links_count
                 }
                 documents.append(doc)
         solrq.solr_index_documents(documents)
@@ -43,19 +47,17 @@ def process_batch(offset):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Index the labels of all qids from wikidata into Solr')
     parser.add_argument('--batch_size', type=int, default=batch_size, help='Size of the batch to process')
-    #parser.add_argument('--max', type=int, help='Max number of qids to process, if not set, all qids will be processed')
     parser.add_argument('--workers', type=int, default=1, help='Number of workers to use')
     args = parser.parse_args()
+    batch_size=args.batch_size
     total_qids=sq.count_qids()
     print(f"Total qids in Wikidata: {total_qids}")
-    #max_test=100000 # TODO: remove this line
-    #max_qids=min(total_qids,max_test)
+    #max_qids=min(total_qids,1000) # TODO: remove this line
     max_qids=total_qids
     arguments = [(offset) for offset in range(0, max_qids, args.batch_size)]
     workers=args.workers
     #workers=4 # TODO: remove this line
     with Pool(workers) as p:
-        #p.map(process_batch, arguments)
         for _ in tqdm(p.imap_unordered(process_batch, arguments), total=len(arguments)):
             pass
     solrq.solr_commit()
